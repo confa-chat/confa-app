cfc1f82d60891cab477c0a2280bc3b7d522fba5d
royalcat <k.adamovich20@gmail.com>
2025-05-19 00:20:34 +0400

    fix

--- lib/router.dart
+++ lib/router.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:konfa/screens/connect_screen.dart';
 import 'package:konfa/screens/server_screen.dart';
 import 'package:konfa/screens/server_selection_screen.dart';
 
 /// Combines all route definitions from across the app
 List<RouteBase> get $appRoutes => [
+  $connectScreenRoute,
   $serverSelectionScreenRoute,
   $serverScreenRoute,
-  // $loadingScreenRoute,
 ];
 
 /// The main router for the app
